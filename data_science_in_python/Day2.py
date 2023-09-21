1dcd97faeaf7bef95b2afb24460133eeccee253e
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-21 23:03:34 +0530

    Day2-modified

--- data_science_in_python/Day2.py
+++ data_science_in_python/Day2.py
@@ -3,3 +3,17 @@ import numpy as np
 # Copy() Vs View()
 a = np.array([1, 2, 3, 4, 5])
 print(a)
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+b = a.copy()  # copies the value from one array to another
+print(b)
+a[0] = 12
+print(a)
+print(b)
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+b = a.view()  # share the memory address
+print(b)
+a[0] = 12
+print(a)
+print(b)
