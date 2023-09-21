0a0e0eb4cc34e514cb44f0df5e75432207435df1
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-21 23:05:59 +0530

    Day-2 modified

--- data_science_in_python/Day2.py
+++ data_science_in_python/Day2.py
@@ -17,3 +17,10 @@ print(b)
 a[0] = 12
 print(a)
 print(b)
+# Shape of an array
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 5, 3]])
+print(a)
+print(a.shape)  # shape - no.of rows and columns in a given array
+b = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]], [[2, 4], [6, 8]]])
+print(b)
+print(b.shape)
