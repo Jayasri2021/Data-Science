aaf6279896fa148583c77993494b405e75f57a4f
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-22 08:36:23 +0530

    Day-3 modified

--- data_science_in_python/Day3.py
+++ data_science_in_python/Day3.py
@@ -15,3 +15,10 @@ ser_1 = pd.Series(list, index=["A", "B", "C", "D", "E"])
 print(ser_1)
 print(ser_1["C"])
 print(ser_1["A"])
+# DICTIONARY
+dt = {"General-Knowledge": 98, "English": 89, "Maths": 85, "Science": 96, "Social": 98}
+print(dt)
+ser_2 = pd.Series(dt)
+print(ser_2)
+ser_3 = pd.Series(dt, index=["Maths", "Science", "Social"])
+print(ser_3)
