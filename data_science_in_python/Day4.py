d2abd692e5a9d519a6e9acc6bd160baede52b047
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-24 09:34:28 +0530

    Day-4-Modified

--- data_science_in_python/Day4.py
+++ data_science_in_python/Day4.py
@@ -5,33 +5,38 @@ dt = pd.read_csv("u.user.csv")
 cols = ["User_id", "Age", "Gender", "Occupation", "Zipcode"]
 dt = pd.read_csv("u.user.csv", sep="|", names=cols)
 print(dt)
-dt.info()
-dt.describe  # numerics
-dt["Gender"]
-dt.nunique()
-dt["Gender"].value_counts()
-dt[(dt["Age"] > 30) & (dt["Gender"] == "F")]
-dt[(dt["Age"] > 30) & (dt["Gender"] == "M")]
-dt.loc[100:110]
-dt.iloc[100:110]
-dt.loc[100:110, "Gender"]
-dt.iloc[100:110, 3]
-dt.iloc[100:110, 0:3]
+print(dt.info())
+print(dt.describe)  # numerics
+print(dt["Gender"])
+print(dt.nunique())
+print(dt["Gender"].value_counts())
+print(dt[(dt["Age"] > 30) & (dt["Gender"] == "F")])
+print(dt[(dt["Age"] > 30) & (dt["Gender"] == "M")])
+print(dt.loc[100:110])
+print(dt.iloc[100:110])
+print(dt.loc[100:110, "Gender"])
+print(dt.iloc[100:110, 3])
+print(dt.iloc[100:110, 0:3])
 
 # Converters
 
 data = pd.read_csv("city-of-chicago-salaries.csv")
-data
-data.info()
-data.describe
-data.head()
-data.tail()
-data.nunique()
-data["Department"].value_counts()
-data["Department"].unique()  # shows only the fields and not the count
+print(data)
+print(data.info())
+print(data.describe)
+print(data.head())
+print(data.tail())
+print(data.nunique())
+print(data["Department"].value_counts())
+print(data["Department"].unique())  # shows only the fields and not the count
 # remove the dollar symbols
 data = pd.read_csv(
     "city-of-chicago-salaries.csv",
     converters={"Employee Annual Salary": lambda x: float(x.replace("$", " "))},
 )
-data
+print(data)
+data = pd.read_csv(
+    "./Downloads/city-of-chicago-salaries.csv",
+    converters={"Department": lambda x: (x.replace("POLICE", "TRAINER"))},
+)
+print(data)
