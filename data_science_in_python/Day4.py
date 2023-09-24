63e58fd0183243a00eb33fa82280bc9fd4aba69e
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-24 09:07:27 +0530

    Day-4-Modified

---
+++ data_science_in_python/Day4.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+dt = pd.read_csv("./Downloads/u.user")
+cols = ["User_id", "Age", "Gender", "Occupation", "Zipcode"]
+dt = pd.read_csv("./Downloads/u.user", sep="|", names=cols)
+print(dt)
+dt.info()
+dt.describe  # numerics
+dt["Gender"]
+dt.nunique()
+dt["Gender"].value_counts()
