68e92efaf24505739778e3158aa2e5713c596bc1
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-27 05:33:59 +0530

    Day5-modified

--- data_science_in_python/Day5.py
+++ data_science_in_python/Day5.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("./Downloads/u.user")
+data = pd.read_csv("u.user.csv")
 data
 cols = ["User_id", "Age", "Gender", "Occupation", "Zipcode"]
-data = pd.read_csv("./Downloads/u.user", sep="|", names=cols)
+data = pd.read_csv("u.user.csv", sep="|", names=cols)
 print(data)
 data.info()
+data.head()
+data.tail()
