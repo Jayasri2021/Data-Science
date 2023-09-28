52b9e7667ef81684b7d9ef10719f635f115602c6
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-28 21:08:41 +0530

    Day-6-Modified

--- data_science_in_python/Day6.py
+++ data_science_in_python/Day6.py
@@ -2,4 +2,8 @@ import numpy as np
 import pandas as pd
 
 x = pd.DataFrame({"key": range(0, 5), "leftval": [10, 20, 30, 40, 50]})
-x
+print(x)
+y = pd.DataFrame({"key": range(2, 7), "rightval": [60, 70, 80, 90, 100]})
+print(y)
+z = pd.merge(x, y)
+print(z)
