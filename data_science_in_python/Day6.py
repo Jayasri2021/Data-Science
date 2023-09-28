59ebb925ee5a1e297799ca85453b351bedba0274
Jayasri <jayasri.suresh2021@gmail.com>
2023-09-28 21:15:01 +0530

    Day-6-Modified

--- data_science_in_python/Day6.py
+++ data_science_in_python/Day6.py
@@ -17,3 +17,11 @@ z3 = pd.merge(x, y, how="outer")
 print(z3)
 x3 = pd.DataFrame({"s_no": [1, 2, 3, 4], "value1": [20, 30, 40, 50]})
 print(x3)
+x4 = pd.DataFrame({"serial_no": [1, 2, 3, 4], "value2": [20, 30, 40, 50]})
+print(x4)
+x5 = pd.merge(x3, x4, left_on="s_no", right_on="serial_no", how="inner")
+print(x5)
+cols1 = ["movie_id", "movie_name", "release_data", "video_release_date", "imdb_url"]
+movies = pd.read_csv(
+    "u.item", sep="|", encoding="latin-1", names=cols1, usecols=range(5)
+)
