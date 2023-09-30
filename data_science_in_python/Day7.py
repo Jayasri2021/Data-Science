62ac6abdb06ca1afdcb3a523574fdc81138e21ee
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-01 01:10:53 +0530

    Day7

--- data_science_in_python/Day7.py
+++ data_science_in_python/Day7.py
@@ -5,6 +5,6 @@ cols = ["user_id", "age", "gender", "occupation", "zipcode"]
 dt = pd.read_csv("./Downloads/u.user", sep="|", names=cols)
 print(dt.head(10))
 grp_gen = dt.groupby("gender").count()
-grp_gen
+print(grp_gen)
 AV = pd.read_csv("./Downloads/city-of-chicago-salaries.csv")
-AV
+print(AV)
