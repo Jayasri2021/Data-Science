5716026726083155702faa55bfd48dace99dffb7
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-01 01:14:52 +0530

    Day7

--- data_science_in_python/Day7.py
+++ data_science_in_python/Day7.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 cols = ["user_id", "age", "gender", "occupation", "zipcode"]
-dt = pd.read_csv("./Downloads/u.user", sep="|", names=cols)
+dt = pd.read_csv("u.user", sep="|", names=cols)
 print(dt.head(10))
 grp_gen = dt.groupby("gender").count()
 print(grp_gen)
