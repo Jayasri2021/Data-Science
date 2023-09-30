923bf995d5a7420793c4219e64d026accbb721c0
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-01 01:07:30 +0530

    Day7-Modified

--- data_science_in_python/Day7.py
+++ data_science_in_python/Day7.py
@@ -4,3 +4,7 @@ import pandas as pd
 cols = ["user_id", "age", "gender", "occupation", "zipcode"]
 dt = pd.read_csv("./Downloads/u.user", sep="|", names=cols)
 print(dt.head(10))
+grp_gen = dt.groupby("gender").count()
+grp_gen
+AV = pd.read_csv("./Downloads/city-of-chicago-salaries.csv")
+AV
