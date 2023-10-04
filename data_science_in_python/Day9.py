30b02f8bd2f8482334302b3873c93423a0177e05
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-04 19:30:24 +0530

    Day-9 Modified

--- data_science_in_python/Day9.py
+++ data_science_in_python/Day9.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
 
-am = pd.read_csv("./Downloads/automobile.csv")
+am = pd.read_csv("automobile.csv")
 print(am)
 auto_temp1 = am[["make", "price"]]
 group1 = auto_temp1.groupby(["make"]).mean().sort_values(ascending=False, by="price")
@@ -13,3 +13,6 @@ group1.plot.bar()
 temp_df = am[["make", "price"]].groupby(["make"])
 temp_df.head()
 stats.f_oneway(temp_df.get_group("audi")["price"], temp_df.get_group("volvo")["price"])
+stats.f_oneway(
+    temp_df.get_group("jaguar")["price"], temp_df.get_group("honda")["price"]
+)
