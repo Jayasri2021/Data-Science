8cc46cc5ac2361f534c930c5b2f0b014d3387c93
Jayasri <jayasri.suresh2021@gmail.com>
2023-10-04 19:26:01 +0530

    Day-9 modified

--- data_science_in_python/Day9.py
+++ data_science_in_python/Day9.py
@@ -3,3 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
+
+am = pd.read_csv("./Downloads/automobile.csv")
+print(am)
+auto_temp1 = am[["make", "price"]]
+group1 = auto_temp1.groupby(["make"]).mean().sort_values(ascending=False, by="price")
+print(group1)
